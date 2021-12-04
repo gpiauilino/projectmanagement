b555e7e36c79e586ce57b76390bc51539b35f570
Gabriel <gabrielpiauilinoalmeida@gmail.com>
2021-12-04 20:58:28 -0300

    formatacao

--- src/gui/Inicio.java
+++ src/gui/Inicio.java
@@ -9,7 +9,9 @@ public class Inicio {
 
 
     public static void main(String args[]) {
+
         Utilitarios utils = new Utilitarios();
+
         java.awt.EventQueue.invokeLater(() -> {
             new TelaPrincipal(utils).setVisible(true);
         });
