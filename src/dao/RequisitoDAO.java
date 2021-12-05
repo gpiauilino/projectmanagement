3652e883dcb79d6bbdc5a7c35cf42ab3ec461906
Gabriel <gabrielpiauilinoalmeida@gmail.com>
2021-12-04 22:57:15 -0300

    reorganizei, faltavam referencias de colunas

--- src/dao/RequisitoDAO.java
+++ src/dao/RequisitoDAO.java
@@ -1,7 +1,5 @@
 package dao;
 
-import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.time;
-import factory.ConnectionFactory;
 import modelo.RequisitosModel;
 import java.sql.*;
 import java.sql.PreparedStatement;
@@ -11,93 +9,80 @@ import javax.swing.JOptionPane;
 public class RequisitoDAO {
 
     private final Connection connection;
-    private long idRequisito;
-    private long usuario_id;
-    private long projeto_id;
-    private String nomeRequisito;
-    private String nomeProjeto;
-    private String modulo;
-    private String autor;
-    private String funcionalidades; 
-    private String autorUltimaMod;
-    private String descricao;
-    private Double versao;
-    private Double esforcoHoras;
-    private int prioridade;
-    private int estado;
-    private int fase;
-    private int complexidade;
-    private java.util.Date dataCriacao;
-    private java.util.Date dataUltimaMod;
-    
+
     public RequisitoDAO(Connection con) {
         this.connection = con;
     }
 
     public void salvar(RequisitosModel objRequisito) {
-        
-        //String strGetIdProjeto = String.valueOf(objRequisito.getId());
-        
+
         try {
             String sql;
-            if (String.valueOf(objRequisito.getIdRequisito()).isEmpty()) {
-                sql = "INSERT INTO requisito(nome,descricao, usuario_id, projeto_id, modulo, funcionalidades, versao, esforcoHoras, prioridade, estado, fase, complexidade, dataCriacao, dataUltimamod) VALUES(?,?,?)";
-                PreparedStatement stmt = connection.prepareStatement(sql);
 
-                stmt.setLong(1, objRequisito.getIdRequisito());
-                stmt.setLong(2, objRequisito.getUsuario_id());
-                stmt.setLong(3, objRequisito.getProjeto_id());
-                stmt.setString(4, objRequisito.getNomeRequisito());
-                stmt.setString(5, objRequisito.getModulo());
-                stmt.setString(6, objRequisito.getFuncionalidades());
-                stmt.setString(7, objRequisito.getDescricao());
-                stmt.setDouble(8, objRequisito.getVersao());
-                stmt.setDouble(9, objRequisito.getEsforcoHoras());
-                stmt.setInt(10, objRequisito.getPrioridade());
-                stmt.setInt(11, objRequisito.getEstado());
-                stmt.setInt(12, objRequisito.getFase());
-                stmt.setInt(13, objRequisito.getComplexidade());
-                stmt.setDate(14, (Date) objRequisito.getDataCriacao());
-                stmt.setDate(15, (Date) objRequisito.getDataUltimaMod());
-                
-                             
-                
-            
-                
-             
-                //stmt.setString(4, objRequisito.getTelefone());
+            // campos do tipo Long, como os IDs devem ser comparados usando long n como String
+            if (objRequisito.getId() > 0) {
+                sql = "INSERT INTO requisito("
+                        + "nome,"
+                        + "descricao,"
+                        + "modulo,"
+                        + "funcionalidades,"
+                        + "versao,"
+                        + "esforcoHoras,"
+                        + "prioridade, "
+                        + "estado,"
+                        + "fase,"
+                        + "complexidade,"
+                        + "usuario_id,"
+                        + "projeto_id) "
+                        + " VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+
+                PreparedStatement stmt = connection.prepareStatement(sql);
+                stmt.setString(1, objRequisito.getNomeRequisito());
+                stmt.setString(2, objRequisito.getDescricao());
+                stmt.setString(3, objRequisito.getModulo());
+                stmt.setString(4, objRequisito.getFuncionalidades());
+                stmt.setDouble(5, objRequisito.getVersao());
+                stmt.setDouble(6, objRequisito.getEsforcoHoras());
+                stmt.setInt(7, objRequisito.getPrioridade());
+                stmt.setInt(8, objRequisito.getEstado());
+                stmt.setInt(9, objRequisito.getFase());
+                stmt.setInt(10, objRequisito.getComplexidade());
+                stmt.setLong(11, objRequisito.getUsuario_id());
+                stmt.setLong(12, objRequisito.getProjeto_id());
+
                 stmt.execute();
-                
 
             } else {
                 sql = "UPDATE requisito "
-                        + "SET nome = ?, descricao = ?, modulo = ?, funcionalidades = ?, versao = ?, esforco = ?, prioridade = ?, estado = ?, fase = ?, complexidade = ?, data criacao = ?, data ultima modificacao = ?, WHERE requisito.id = ?";
-                
-               
+                        + "SET nome = ?, "
+                        + "descricao = ?, "
+                        + "modulo = ?, "
+                        + "funcionalidades = ?, "
+                        + "versao = ?, "
+                        + "esforco = ?, "
+                        + "prioridade = ?, "
+                        + "estado = ?, "
+                        + "fase = ?, "
+                        + "complexidade = ?, "
+                        + "usuario_id = ?, "
+                        + "projeto_id = ? "
+                        + " WHERE requisito.id = ?";
 
                 PreparedStatement stmt = connection.prepareStatement(sql);
-      stmt.setString(1, objRequisito.getNomeRequisito());
+                stmt.setString(1, objRequisito.getNomeRequisito());
                 stmt.setString(2, objRequisito.getDescricao());
-                
-                //TODO demais campos faltam aqui
-                               
-                stmt.setLong(3, objRequisito.getIdRequisito());
-                stmt.setLong(4, objRequisito.getUsuario_id());
-                stmt.setLong(5, objRequisito.getProjeto_id());
-                stmt.setString(6, objRequisito.getNomeRequisito());
-                stmt.setString(7, objRequisito.getModulo());
-                stmt.setString(8, objRequisito.getFuncionalidades());
-                stmt.setString(9, objRequisito.getDescricao());
-                stmt.setDouble(10, objRequisito.getVersao());
-                stmt.setDouble(11, objRequisito.getEsforcoHoras());
-                stmt.setInt(12, objRequisito.getPrioridade());
-                stmt.setInt(13, objRequisito.getEstado());
-                stmt.setInt(14, objRequisito.getFase());
-                stmt.setInt(15, objRequisito.getComplexidade());
-                stmt.setDate(16, (Date) objRequisito.getDataCriacao());
-                stmt.setDate(17, (Date) objRequisito.getDataUltimaMod());
-                           
-                                
+                stmt.setString(3, objRequisito.getModulo());
+                stmt.setString(4, objRequisito.getFuncionalidades());
+                stmt.setDouble(5, objRequisito.getVersao());
+                stmt.setDouble(6, objRequisito.getEsforcoHoras());
+                stmt.setInt(7, objRequisito.getPrioridade());
+                stmt.setInt(8, objRequisito.getEstado());
+                stmt.setInt(9, objRequisito.getFase());
+                stmt.setInt(10, objRequisito.getComplexidade());
+                stmt.setLong(11, objRequisito.getUsuario_id());
+                stmt.setLong(12, objRequisito.getProjeto_id());
+                stmt.setLong(13, objRequisito.getId());
+
                 // Execute
                 stmt.execute();
 
@@ -111,7 +96,7 @@ public class RequisitoDAO {
         try {
             String sql = "";
             if (!objRequisito.getNomeRequisito().isEmpty()) {
-                sql = "SELECT * FROM requisito WHERE nome LIKE '%" + objRequisito.getNomeRequisito()+ "%' ";
+                sql = "SELECT * FROM requisito WHERE nome LIKE '%" + objRequisito.getNomeRequisito() + "%' ";
 
             } else if (!objRequisito.getDescricao().isEmpty()) {
                 sql = "SELECT * FROM requisito WHERE descricao LIKE '%" + objRequisito.getDescricao() + "%' ";
@@ -126,7 +111,7 @@ public class RequisitoDAO {
                 dado.add(new Object[]{
                     rs.getLong("id"),
                     rs.getLong("usuario"),
-                    rs.getLong("projeto"),                    
+                    rs.getLong("projeto"),
                     rs.getString("nome"),
                     rs.getString("modulo"),
                     rs.getString("funcionalidades"),
@@ -134,43 +119,38 @@ public class RequisitoDAO {
                     rs.getDouble("versao"),
                     rs.getDouble("esforco"),
                     rs.getInt("prioridade"),
-                    rs.getInt("prioridade"),
                     rs.getInt("estado"),
                     rs.getInt("fase"),
                     rs.getInt("complexidade"),
-                    rs.getTimestamp("Data criacao"),
-                    rs.getTimestamp("Data Ultima Modificacao"),         
-                
-                    
+                    rs.getTimestamp("data_criacao"),
+                    rs.getTimestamp("data_modificacao")
                 });
 
             }
             ps.close();
             rs.close();
-            //connection.close()
 
             return dado;
         } catch (SQLException e) {
             e.getMessage();
-            JOptionPane.showMessageDialog(null, "Erro preencher o ArrayList");
+            JOptionPane.showMessageDialog(null, "Erro preencher o ArrayList: " + e.getMessage());
             return null;
         }
 
     }
 
     public void deletar(RequisitosModel objRequisito) {
-        
-    String strGetIdProjeto = String.valueOf(objRequisito.getIdRequisito());
-    
+
+        long id = objRequisito.getId();
+
         try {
             String sql;
-            if (!String.valueOf(strGetIdProjeto).isEmpty()) {
+            if (id > 0) {
                 sql = "DELETE FROM requisito WHERE requisito.id = ?";
                 PreparedStatement stmt = connection.prepareStatement(sql);
 
-                stmt.setString(1, strGetIdProjeto);
+                stmt.setLong(1, id);
                 stmt.execute();
-                
 
             }
         } catch (SQLException u) {
@@ -185,14 +165,11 @@ public class RequisitoDAO {
 
             PreparedStatement ps = connection.prepareStatement("SELECT * FROM requisito");
             ResultSet rs = ps.executeQuery();
-
-           
             while (rs.next()) {
-                
                 dado.add(new Object[]{
                     rs.getLong("id"),
                     rs.getLong("usuario"),
-                    rs.getLong("projeto"),                    
+                    rs.getLong("projeto"),
                     rs.getString("nome"),
                     rs.getString("modulo"),
                     rs.getString("funcionalidades"),
@@ -200,19 +177,16 @@ public class RequisitoDAO {
                     rs.getDouble("versao"),
                     rs.getDouble("esforco"),
                     rs.getInt("prioridade"),
-                    rs.getInt("prioridade"),
                     rs.getInt("estado"),
                     rs.getInt("fase"),
                     rs.getInt("complexidade"),
-                    rs.getTimestamp("Data criacao"),
-                    rs.getTimestamp("Data Ultima Modificacao"),
-                  
+                    rs.getTimestamp("data_criacao"),
+                    rs.getTimestamp("data_modificacao")
                 });
 
             }
             ps.close();
             rs.close();
-            //connection.close()
 
             return dado;
         } catch (SQLException e) {
