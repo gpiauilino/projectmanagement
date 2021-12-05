bb39af9b28d3ca890610e47032afcd07efba7b48
Gabriel <gabrielpiauilinoalmeida@gmail.com>
2021-12-04 21:00:46 -0300

    alterado id de string para long e revisado todos os campos e colunas

--- src/dao/ProjetoDAO.java
+++ src/dao/ProjetoDAO.java
@@ -1,6 +1,5 @@
 package dao;
 
-import factory.ConnectionFactory;
 import modelo.ProjetoModel;
 import java.sql.*;
 import java.sql.PreparedStatement;
@@ -10,46 +9,33 @@ import javax.swing.JOptionPane;
 public class ProjetoDAO {
 
     private final Connection connection;
-    private long id;
-    private long id_usu;
-    private String nome;
-    private String descricao;
-    private String data_criacao;
-    private String data_modificacao;
-    private String autor;
 
-    
     public ProjetoDAO(Connection con) {
         this.connection = con;
     }
 
     public void salvar(ProjetoModel objProjeto) {
-        
-        //String strGetIdProjeto = String.valueOf(objProjeto.getId());
-        
+        System.out.println("Salvar Projeto");
         try {
             String sql;
-            if (String.valueOf(objProjeto.getId()).isEmpty()) {
-                sql = "INSERT INTO projeto(nome,descricao, id_usuario) VALUES(?,?,?)";
+            if (objProjeto.getId() == 0l) {
+                sql = "INSERT INTO projeto(nome,descricao, usuario_id) VALUES(?,?,?)";
                 PreparedStatement stmt = connection.prepareStatement(sql);
 
                 stmt.setString(1, objProjeto.getNome());
                 stmt.setString(2, objProjeto.getDescricao());
                 stmt.setLong(3, objProjeto.getId_usu());
-                //stmt.setString(4, objProjeto.getTelefone());
                 stmt.execute();
-                
 
             } else {
-                sql = "UPDATE projeto "
-                        + "SET nome = ?, descricao = ? WHERE projeto.id = ?";
+                sql = "UPDATE projeto SET nome = ?, descricao = ?, usuario_id WHERE projeto.id = ?";
 
                 PreparedStatement stmt = connection.prepareStatement(sql);
-      stmt.setString(1, objProjeto.getNome());
+                stmt.setString(1, objProjeto.getNome());
                 stmt.setString(2, objProjeto.getDescricao());
+                stmt.setLong(3, objProjeto.getId_usu());
+                stmt.setLong(4, objProjeto.getId());
                 
-                //TODO demais campos faltam aqui
-                stmt.setLong(3, objProjeto.getId());
                 stmt.execute();
 
             }
@@ -67,46 +53,44 @@ public class ProjetoDAO {
             } else if (!objProjeto.getDescricao().isEmpty()) {
                 sql = "SELECT * FROM projeto WHERE descricao LIKE '%" + objProjeto.getDescricao() + "%' ";
             }
-            ArrayList dado = new ArrayList();
+            ArrayList lista_de_projetos = new ArrayList();
 
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-
-                dado.add(new Object[]{
-                    rs.getLong("id"),
-                    rs.getString("nome"),
-                    rs.getString("descricao"),
-                });
-
+            ResultSet rs;
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    lista_de_projetos.add(new Object[]{
+                        rs.getLong("id"),
+                        rs.getString("nome"),
+                        rs.getString("descricao"),
+                        rs.getString("data_criacao"),
+                        rs.getString("data_modificacao"),
+                        rs.getLong("usuario_id")
+                    });
+                }
             }
-            ps.close();
             rs.close();
-            //connection.close()
 
-            return dado;
+            return lista_de_projetos;
+
         } catch (SQLException e) {
-            e.getMessage();
-            JOptionPane.showMessageDialog(null, "Erro preencher o ArrayList");
+            JOptionPane.showMessageDialog(null, "Erro ao buscar lista de projetos: " + e.getMessage());
             return null;
         }
-
     }
 
     public void deletar(ProjetoModel objProjeto) {
-        
-    String strGetIdProjeto = String.valueOf(objProjeto.getId());
-    
+
+        long idProjeto = objProjeto.getId();
+
         try {
             String sql;
-            if (!String.valueOf(strGetIdProjeto).isEmpty()) {
+            if (idProjeto > 0) {
                 sql = "DELETE FROM projeto WHERE projeto.id = ?";
                 PreparedStatement stmt = connection.prepareStatement(sql);
 
-                stmt.setString(1, strGetIdProjeto);
+                stmt.setLong(1, idProjeto);
                 stmt.execute();
-                
 
             }
         } catch (SQLException u) {
@@ -117,25 +101,27 @@ public class ProjetoDAO {
     public ArrayList listarTodos() {
         try {
 
-            ArrayList dado = new ArrayList();
+            ArrayList lista_de_projetos = new ArrayList();
 
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM projeto");
-            ResultSet rs = ps.executeQuery();
+            ResultSet rs;
+            try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM projeto")) {
+                rs = ps.executeQuery();
+                while (rs.next()) {
 
-            while (rs.next()) {
+                    lista_de_projetos.add(new Object[]{
+                        rs.getLong("id"),
+                        rs.getString("nome"),
+                        rs.getString("descricao"),
+                        rs.getString("data_criacao"),
+                        rs.getString("data_modificacao"),
+                        rs.getLong("usuario_id")
+                    });
 
-                dado.add(new Object[]{
-                    rs.getLong("id"),
-                    rs.getString("nome"),
-                    rs.getString("descricao")
-                });
-
+                }
             }
-            ps.close();
             rs.close();
-            //connection.close()
 
-            return dado;
+            return lista_de_projetos;
         } catch (SQLException e) {
             e.getMessage();
             JOptionPane.showMessageDialog(null, "Erro Exception SQL: " + e.getMessage());
